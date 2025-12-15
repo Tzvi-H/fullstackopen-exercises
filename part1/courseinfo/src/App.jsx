a6d0a141fba350c1701e939bb0210116389b404c
Tzvi H <hershy@Tzvis-MacBook-Air.local>
2025-12-15 14:38:04 -0600

    1.5

--- part1/courseinfo/src/App.jsx
+++ part1/courseinfo/src/App.jsx
@@ -19,35 +19,37 @@ const Content = props => {
   )
 }
 
-const Total = props => {
-  const [part1, part2, part3] = props.parts
+const Total = ({parts}) => {
+  const [part1, part2, part3] = parts
   const total = part1.exercises + part2.exercises + part3.exercises
 
   return <p>Number of exercises {total}</p>
 }
 
 const App = () => {
-  const course = 'Half Stack application development'
-  const parts = [
-    {
-      name: 'Fundamentals of React',
-      exercises: 10
-    },
-    {
-      name: 'Using props to pass data',
-      exercises: 7
-    },
-    {
-      name: 'State of a component',
-      exercises: 14
-    }
-  ]
+  const course = {
+    name: 'Half Stack application development',
+    parts: [
+      {
+        name: 'Fundamentals of React',
+        exercises: 10
+      },
+      {
+        name: 'Using props to pass data',
+        exercises: 7
+      },
+      {
+        name: 'State of a component',
+        exercises: 14
+      }
+    ]
+  }
 
   return (
     <div>
-      <Header course={course} />
-      <Content parts={parts} />
-      <Total parts={parts} />
+      <Header course={course.name} />
+      <Content parts={course.parts} />
+      <Total parts={course.parts} />
     </div>
   )
 }
