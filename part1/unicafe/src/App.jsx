2bdb160facb8ae5a7afc42f7365a7a2af7998ddf
Tzvi H <16guitar@gmail.com>
2025-12-15 16:12:45 -0600

    1.11

--- part1/unicafe/src/App.jsx
+++ part1/unicafe/src/App.jsx
@@ -2,7 +2,12 @@ import { useState } from 'react'
 
 const Button = ({text, onClick}) => <button onClick={onClick}>{text}</button>
 
-const StatisticLine = ({text, value}) => <span>{text} {value}<br/></span>
+const StatisticLine = ({text, value}) => (
+  <tr>
+    <td>{text}</td>
+    <td>{value}</td>
+  </tr>
+)
 
 const Feedback = (props) => {
   return (
@@ -18,7 +23,7 @@ const Feedback = (props) => {
 const Statistics = ({good, neutral, bad}) => {
   const total = good + neutral + bad
   const average = ((good * 1 + bad * -1) / total) || 0
-  const positive = (good / total) || 0
+  const positive = (good / total) * 100 || 0
 
   if (total === 0) {
     return (
@@ -32,14 +37,16 @@ const Statistics = ({good, neutral, bad}) => {
   return (
     <div>
       <h1>statistics</h1>
-      <p>
-        <StatisticLine text="good" value={good}/>
-        <StatisticLine text="neutral" value={neutral}/>
-        <StatisticLine text="bad" value={bad}/>
-        <StatisticLine text="all" value={total}/>
-        <StatisticLine text="average" value={average}/>
-        <StatisticLine text="positive" value={`${positive} %`}/>
-      </p>
+      <table>
+        <tbody>
+          <StatisticLine text="neutral" value={neutral}/>
+          <StatisticLine text="good" value={good}/>
+          <StatisticLine text="bad" value={bad}/>
+          <StatisticLine text="all" value={total}/>
+          <StatisticLine text="average" value={average}/>
+          <StatisticLine text="positive" value={`${positive} %`}/>
+        </tbody>
+      </table>
     </div>
   )
 }
