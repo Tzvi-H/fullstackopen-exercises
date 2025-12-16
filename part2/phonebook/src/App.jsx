b24afbf8fe8d2c0ab3c2366c0d8fbc5e10e0665b
Tzvi H <16guitar@gmail.com>
2025-12-16 12:51:14 -0600

    2.8

--- part2/phonebook/src/App.jsx
+++ part2/phonebook/src/App.jsx
@@ -1,10 +1,14 @@
 import { useState } from "react";
 
 const App = () => {
-  const [persons, setPersons] = useState([{ name: "Arto Hellas" }]);
+  const [persons, setPersons] = useState([
+    { name: "Arto Hellas", number: "040-1234567" },
+  ]);
   const [newName, setNewName] = useState("");
+  const [newNumber, setNewNumber] = useState("");
 
   const handleNameChange = (e) => setNewName(e.target.value);
+  const handleNumberChange = (e) => setNewNumber(e.target.value);
 
   const handleFormSubmit = (e) => {
     e.preventDefault();
@@ -16,9 +20,11 @@ const App = () => {
 
     const newPerson = {
       name: newName,
+      number: newNumber,
     };
     setPersons([...persons, newPerson]);
     setNewName("");
+    setNewNumber("");
   };
 
   return (
@@ -29,13 +35,16 @@ const App = () => {
           name: <input value={newName} onChange={handleNameChange} />
         </div>
         <div>
+          number: <input value={newNumber} onChange={handleNumberChange} />
+        </div>
+        <div>
           <button type="submit">add</button>
         </div>
       </form>
       <h2>Numbers</h2>
       {persons.map((person) => (
         <span key={person.name}>
-          {person.name}
+          {person.name} {person.number}
           <br />
         </span>
       ))}
