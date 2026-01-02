6d92ae251abb4b678888ef97920da4bd8436b195
Tzvi H <16guitar@gmail.com>
2026-01-02 17:27:46 -0600

    3.20

--- part3/phonebook/models/person.js
+++ part3/phonebook/models/person.js
@@ -22,7 +22,17 @@ const personSchema = new mongoose.Schema({
     minlength: 3,
     required: true,
   },
-  number: String,
+  number: {
+    type: String,
+    minlength: 8,
+    required: true,
+    validate: {
+      validator: function (v) {
+        return /^\d{2,3}-\d+$/.test(v);
+      },
+      message: (props) => `${props.value} is not a valid phone number`,
+    },
+  },
 });
 
 personSchema.set("toJSON", {
