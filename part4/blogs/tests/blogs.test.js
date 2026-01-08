e245fa49ddb90757e7c470bf56bc38cf7161591e
Tzvi H <16guitar@gmail.com>
2026-01-08 11:57:58 -0600

    4.8

--- part4/blogs/tests/blogs.test.js
+++ part4/blogs/tests/blogs.test.js
@@ -1,274 +1,38 @@
-const { test, describe } = require("node:test");
+const { test, after, beforeEach, describe } = require("node:test");
 const assert = require("node:assert");
-const listHelper = require("../utils/list_helper");
+const supertest = require("supertest");
+const app = require("../app");
+const mongoose = require("mongoose");
+const Blog = require("../models/blog");
+const helper = require("./test_helper");
 
-test("dummy returns one", () => {
-  const blogs = [];
+const api = supertest(app);
 
-  const result = listHelper.dummy(blogs);
-  assert.strictEqual(result, 1);
+beforeEach(async () => {
+  await Blog.deleteMany({});
+  await Blog.insertMany(helper.initialBlogs);
 });
 
-describe("total likes", () => {
-  const listWithOneBlog = [
-    {
-      _id: "5a422aa71b54a676234d17f8",
-      title: "Go To Statement Considered Harmful",
-      author: "Edsger W. Dijkstra",
-      url: "https://homepages.cwi.nl/~storm/teaching/reader/Dijkstra68.pdf",
-      likes: 5,
-      __v: 0,
-    },
-  ];
-
-  const listWithManyBlogs = [
-    {
-      _id: "5a422aa71b54a676234d17f8",
-      title: "Go To Statement Considered Harmful",
-      author: "Edsger W. Dijkstra",
-      url: "https://homepages.cwi.nl/~storm/teaching/reader/Dijkstra68.pdf",
-      likes: 5,
-      __v: 0,
-    },
-    {
-      _id: "5a422aa71b54a676234d17f8",
-      title: "Go To Statement Considered Harmful",
-      author: "Edsger W. Dijkstra",
-      url: "https://homepages.cwi.nl/~storm/teaching/reader/Dijkstra68.pdf",
-      likes: 10,
-      __v: 0,
-    },
-    {
-      _id: "5a422aa71b54a676234d17f8",
-      title: "Go To Statement Considered Harmful",
-      author: "Edsger W. Dijkstra",
-      url: "https://homepages.cwi.nl/~storm/teaching/reader/Dijkstra68.pdf",
-      likes: 15,
-      __v: 0,
-    },
-  ];
-
-  test("of an empty list is zero", () => {
-    const result = listHelper.totalLikes([]);
-    assert.strictEqual(result, 0);
+describe("calling /api/blogs", () => {
+  test("blogs are returned as json", async () => {
+    await api
+      .get("/api/blogs")
+      .expect(200)
+      .expect("Content-Type", /application\/json/);
   });
 
-  test("when a list has only one blog, equals the likes of that blog", () => {
-    const result = listHelper.totalLikes(listWithOneBlog);
-    assert.strictEqual(result, 5);
+  test("all blogs are returned", async () => {
+    const response = await api.get("/api/blogs");
+    assert.strictEqual(response.body.length, helper.initialBlogs.length);
   });
 
-  test("of a bigger list is calculated correctly", () => {
-    const result = listHelper.totalLikes(listWithManyBlogs);
-    assert.strictEqual(result, 30);
+  test("a specific blog is returned", async () => {
+    const response = await api.get("/api/blogs");
+    const titles = response.body.map((note) => note.title);
+    assert(titles.includes("title 1"));
   });
 });
 
-describe("favorite blog", () => {
-  const listWithOneBlog = [
-    {
-      _id: "5a422aa71b54a676234d17f8",
-      title: "Go To Statement Considered Harmful",
-      author: "Edsger W. Dijkstra",
-      url: "https://homepages.cwi.nl/~storm/teaching/reader/Dijkstra68.pdf",
-      likes: 5,
-      __v: 0,
-    },
-  ];
-
-  const listWithManyBlogs = [
-    {
-      _id: "5a422aa71b54a676234d17f8",
-      title: "Go To Statement Considered Harmful",
-      author: "Edsger W. Dijkstra",
-      url: "https://homepages.cwi.nl/~storm/teaching/reader/Dijkstra68.pdf",
-      likes: 5,
-      __v: 0,
-    },
-    {
-      _id: "5a422aa71b54a676234d17f8",
-      title: "Go To Statement Considered Harmful",
-      author: "Edsger W. Dijkstra",
-      url: "https://homepages.cwi.nl/~storm/teaching/reader/Dijkstra68.pdf",
-      likes: 10,
-      __v: 0,
-    },
-    {
-      _id: "5a422aa71b54a676234d17f8",
-      title: "Go To Statement Considered Harmful",
-      author: "Edsger W. Dijkstra",
-      url: "https://homepages.cwi.nl/~storm/teaching/reader/Dijkstra68.pdf",
-      likes: 15,
-      __v: 0,
-    },
-  ];
-
-  test("of an empty list is null", () => {
-    const result = listHelper.favoriteBlog([]);
-    assert.strictEqual(result, null);
-  });
-
-  test("when a list has only one blog, equals that blog", () => {
-    const result = listHelper.favoriteBlog(listWithOneBlog);
-    assert.deepStrictEqual(result, listWithOneBlog[0]);
-  });
-
-  test("of a bigger list returns the blog with the most likes", () => {
-    const result = listHelper.favoriteBlog(listWithManyBlogs);
-    assert.deepStrictEqual(result, listWithManyBlogs[2]);
-  });
-});
-
-describe("most blogs", () => {
-  const listWithOneBlog = [
-    {
-      _id: "5a422aa71b54a676234d17f8",
-      title: "Go To Statement Considered Harmful",
-      author: "Edsger W. Dijkstra",
-      url: "https://homepages.cwi.nl/~storm/teaching/reader/Dijkstra68.pdf",
-      likes: 5,
-      __v: 0,
-    },
-  ];
-
-  const listWithManyBlogs = [
-    {
-      _id: "5a422aa71b54a676234d17f8",
-      title: "Go To Statement Considered Harmful",
-      author: "Edsger W. Dijkstra",
-      url: "https://homepages.cwi.nl/~storm/teaching/reader/Dijkstra68.pdf",
-      likes: 5,
-      __v: 0,
-    },
-    {
-      _id: "5a422aa71b54a676234d17f8",
-      title: "Go To Statement Considered Harmful",
-      author: "Edsger W. Dijkstra",
-      url: "https://homepages.cwi.nl/~storm/teaching/reader/Dijkstra68.pdf",
-      likes: 10,
-      __v: 0,
-    },
-    {
-      _id: "5a422aa71b54a676234d17f8",
-      title: "Go To Statement Considered Harmful",
-      author: "random",
-      url: "https://homepages.cwi.nl/~storm/teaching/reader/Dijkstra68.pdf",
-      likes: 10,
-      __v: 0,
-    },
-    {
-      _id: "5a422aa71b54a676234d17f8",
-      title: "Go To Statement Considered Harmful",
-      author: "random",
-      url: "https://homepages.cwi.nl/~storm/teaching/reader/Dijkstra68.pdf",
-      likes: 15,
-      __v: 0,
-    },
-    {
-      _id: "5a422aa71b54a676234d17f8",
-      title: "Go To Statement Considered Harmful",
-      author: "Edsger W. Dijkstra",
-      url: "https://homepages.cwi.nl/~storm/teaching/reader/Dijkstra68.pdf",
-      likes: 15,
-      __v: 0,
-    },
-  ];
-
-  test("of an empty list is null", () => {
-    const result = listHelper.mostBlogs([]);
-    assert.strictEqual(result, null);
-  });
-
-  test("when a list has only one blog, equals that author and and count of 1", () => {
-    const result = listHelper.mostBlogs(listWithOneBlog);
-    assert.deepStrictEqual(result, {
-      author: "Edsger W. Dijkstra",
-      blogs: 1,
-    });
-  });
-
-  test("of a bigger list returns the author with the most blogs", () => {
-    const result = listHelper.mostBlogs(listWithManyBlogs);
-    assert.deepStrictEqual(result, {
-      author: "Edsger W. Dijkstra",
-      blogs: 3,
-    });
-  });
-});
-
-describe("most likes", () => {
-  const listWithOneBlog = [
-    {
-      _id: "5a422aa71b54a676234d17f8",
-      title: "Go To Statement Considered Harmful",
-      author: "Edsger W. Dijkstra",
-      url: "https://homepages.cwi.nl/~storm/teaching/reader/Dijkstra68.pdf",
-      likes: 5,
-      __v: 0,
-    },
-  ];
-
-  const listWithManyBlogs = [
-    {
-      _id: "5a422aa71b54a676234d17f8",
-      title: "Go To Statement Considered Harmful",
-      author: "Edsger W. Dijkstra",
-      url: "https://homepages.cwi.nl/~storm/teaching/reader/Dijkstra68.pdf",
-      likes: 5,
-      __v: 0,
-    },
-    {
-      _id: "5a422aa71b54a676234d17f8",
-      title: "Go To Statement Considered Harmful",
-      author: "Edsger W. Dijkstra",
-      url: "https://homepages.cwi.nl/~storm/teaching/reader/Dijkstra68.pdf",
-      likes: 10,
-      __v: 0,
-    },
-    {
-      _id: "5a422aa71b54a676234d17f8",
-      title: "Go To Statement Considered Harmful",
-      author: "random",
-      url: "https://homepages.cwi.nl/~storm/teaching/reader/Dijkstra68.pdf",
-      likes: 10,
-      __v: 0,
-    },
-    {
-      _id: "5a422aa71b54a676234d17f8",
-      title: "Go To Statement Considered Harmful",
-      author: "random",
-      url: "https://homepages.cwi.nl/~storm/teaching/reader/Dijkstra68.pdf",
-      likes: 15,
-      __v: 0,
-    },
-    {
-      _id: "5a422aa71b54a676234d17f8",
-      title: "Go To Statement Considered Harmful",
-      author: "Edsger W. Dijkstra",
-      url: "https://homepages.cwi.nl/~storm/teaching/reader/Dijkstra68.pdf",
-      likes: 15,
-      __v: 0,
-    },
-  ];
-
-  test("of an empty list is null", () => {
-    const result = listHelper.mostLikes([]);
-    assert.strictEqual(result, null);
-  });
-
-  test("when a list has only one blog, equals that author and and the number of likes", () => {
-    const result = listHelper.mostLikes(listWithOneBlog);
-    assert.deepStrictEqual(result, {
-      author: "Edsger W. Dijkstra",
-      likes: 5,
-    });
-  });
-
-  test("of a bigger list returns the author with the most blogs", () => {
-    const result = listHelper.mostLikes(listWithManyBlogs);
-    assert.deepStrictEqual(result, {
-      author: "Edsger W. Dijkstra",
-      likes: 30,
-    });
-  });
+after(async () => {
+  await mongoose.connection.close();
 });
---
+++ part4/blogs/tests/test_helper.js
@@ -0,0 +1,23 @@
+const Blog = require("../models/blog");
+
+const initialBlogs = [
+  {
+    title: "title 1",
+    author: "author 1",
+    url: "url 1",
+    likes: 1,
+  },
+  {
+    title: "title 2",
+    author: "author 2",
+    url: "url 2",
+    likes: 2,
+  },
+];
+
+const blogsInDb = async () => {
+  const blogs = await Blog.find({});
+  return blogs.map((blog) => blog.toJSON());
+};
+
+module.exports = { initialBlogs, blogsInDb };
