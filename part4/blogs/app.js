86107f2ab4450495f776a11a04e3c7d513ea6c24
Tzvi H <16guitar@gmail.com>
2026-01-08 12:36:37 -0600

    4.12

--- part4/blogs/app.js
+++ part4/blogs/app.js
@@ -9,5 +9,12 @@ mongoose.connect(config.MONGODB_URI, { family: 4 });
 
 app.use(express.json());
 app.use("/api/blogs", blogsRouter);
+app.use((error, req, res, next) => {
+  if (error.name === "ValidationError") {
+    return res.status(400).json({ error: error.message });
+  }
+
+  next(error);
+});
 
 module.exports = app;
--- part4/blogs/tests/blogs.test.js
+++ part4/blogs/tests/blogs.test.js
@@ -6,6 +6,7 @@ const mongoose = require("mongoose");
 const Blog = require("../models/blog");
 const helper = require("./test_helper");
 const { title } = require("node:process");
+const URL = "/api/blogs";
 
 const api = supertest(app);
 
@@ -17,30 +18,30 @@ beforeEach(async () => {
 describe("GET /api/blogs", () => {
   test("blogs are returned as json", async () => {
     await api
-      .get("/api/blogs")
+      .get(URL)
       .expect(200)
       .expect("Content-Type", /application\/json/);
   });
 
   test("all blogs are returned", async () => {
-    const response = await api.get("/api/blogs");
+    const response = await api.get(URL);
     assert.strictEqual(response.body.length, helper.initialBlogs.length);
   });
 
   test("a specific blog is returned", async () => {
-    const response = await api.get("/api/blogs");
+    const response = await api.get(URL);
     const titles = response.body.map((note) => note.title);
     assert(titles.includes("title 1"));
   });
 
   test("a blog does not have the default '_id' property", async () => {
-    const response = await api.get("/api/blogs");
+    const response = await api.get(URL);
     const blog1 = response.body[0];
     assert.strictEqual(blog1._id, undefined);
   });
 
   test("a blog has an 'id' property", async () => {
-    const response = await api.get("/api/blogs");
+    const response = await api.get(URL);
     const blog1 = response.body[0];
     assert.ok(blog1.id);
   });
@@ -56,7 +57,7 @@ describe("POST /api/blogs", () => {
     };
 
     await api
-      .post("/api/blogs")
+      .post(URL)
       .send(newBlog)
       .expect(201)
       .expect("Content-Type", /application\/json/);
@@ -66,6 +67,35 @@ describe("POST /api/blogs", () => {
 
     const titles = blogsAtEnd.map((b) => b.title);
     assert(titles.includes("test title"));
+  });
+
+  test("the default 'likes' property equals 0", async () => {
+    const newBlog = {
+      title: "test title",
+      author: "test author",
+      url: "test url",
+    };
+
+    const { body } = await api.post(URL).send(newBlog);
+    assert.strictEqual(body.likes, 0);
+  });
+
+  test("creating a blog without a title property will result in a 400 response code", async () => {
+    const newBlog = {
+      author: "test author",
+      url: "test url",
+    };
+
+    await api.post(URL).send(newBlog).expect(400);
+  });
+
+  test("creating a blog without a url property will result in a 400 response code", async () => {
+    const newBlog = {
+      title: "test title",
+      author: "test author",
+    };
+
+    await api.post(URL).send(newBlog).expect(400);
   });
 });
 
