3e3455efd972e8c3a3d51abcc29e457a90511441
Tzvi H <16guitar@gmail.com>
2026-01-17 13:46:08 +0530

    4.18

--- part4/blogs/app.js
+++ part4/blogs/app.js
@@ -4,12 +4,14 @@ const config = require("./utils/config");
 const middleware = require("./utils/middleware");
 const blogsRouter = require("./controllers/blogs");
 const usersRouter = require("./controllers/users");
+const loginRouter = require("./controllers/login");
 
 const app = express();
 
 mongoose.connect(config.MONGODB_URI, { family: 4 });
 
 app.use(express.json());
+app.use("/api/login", loginRouter);
 app.use("/api/blogs", blogsRouter);
 app.use("/api/users", usersRouter);
 app.use(middleware.errorHandler);
