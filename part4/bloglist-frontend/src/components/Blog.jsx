5ba27f071c33c35e2bb0bf0a8e31b27f4ab00c6b
Tzvi H <16guitar@gmail.com>
2026-01-21 10:20:33 +0530

    5.7

--- part4/bloglist-frontend/src/components/Blog.jsx
+++ part4/bloglist-frontend/src/components/Blog.jsx
@@ -1,7 +1,35 @@
-const Blog = ({ blog }) => (
-  <div>
-    {blog.title} {blog.author}
-  </div>  
-)
+import { useState } from "react";
 
-export default Blog
+const Blog = ({ blog }) => {
+  const [visible, setVisible] = useState(false);
+
+  const blogStyle = {
+    paddingTop: 10,
+    paddingLeft: 2,
+    border: "solid",
+    borderWidth: 1,
+    marginBottom: 5,
+  };
+
+  const showWhenVisible = { display: visible ? "" : "none" };
+  // const hideWhenVisible = { display: visible ? "none" : "" };
+
+  const toggleVisiblity = () => setVisible(!visible);
+
+  return (
+    <div style={blogStyle}>
+      {blog.title} {blog.author}
+      <button onClick={toggleVisiblity}>{visible ? "hide" : "show"}</button>
+      <div style={showWhenVisible}>
+        {blog.url}
+        <br />
+        likes {blog.likes}
+        <button>like</button>
+        <br />
+        {blog.user.name}
+      </div>
+    </div>
+  );
+};
+
+export default Blog;
