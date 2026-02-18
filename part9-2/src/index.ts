9c2643b911ee473a69402417c5c4af14636e8a5c
Tzvi H <16guitar@gmail.com>
2026-02-18 17:19:33 +0700

    9.10

--- part9-2/src/index.ts
+++ part9-2/src/index.ts
@@ -1,5 +1,6 @@
 import express from "express";
 import cors from "cors";
+import diagnosisRouter from "./routes/diagnoses";
 
 const app = express();
 
@@ -13,9 +14,10 @@ app.use(
 const PORT = 3001;
 
 app.get("/api/ping", (_req, res) => {
-  console.log("someone pinged here");
   res.send("pong");
 });
+
+app.use("/api/diagnoses", diagnosisRouter);
 
 app.listen(PORT, () => {
   console.log(`Server running on port ${PORT}`);
